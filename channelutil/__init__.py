74b62c4dcd25deb97f223ca71c7dd4f41a2e7c5b
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-01 14:11:16 +0000

    Update API and add some doc strings.

--- channelutil/__init__.py
+++ channelutil/__init__.py
@@ -33,88 +33,32 @@ class calculator:
         else:
             return nw.floatList(self.ksigns)
 
-    def e(self, k, primType=False):
+    # Converts free electron wavenumber to energy
+    def fe(self, k):
         ene = (1.0/self.getMult())*k**2
-        if primType:
-            return complex(ene)
-        else:
-            return nw.complex(ene)
+        return nw.complex(ene)
 
-    def fk(self, ene, primType=False): #free k
+    # Converts free electron energy to wavenumber
+    def fk(self, ene): #free k
         k = nw.sqrt(self.getMult()*ene)
-        if primType:
-            return complex(k)
-        else:
-            return nw.complex(k)
+        return nw.complex(k)
 
-    def kl(self, ch, ene, add=0.0):
-        k = self.k(ch, ene)
-        return nw.pow(k, self.ls[ch]+add)
-
+    # Converts channel electron energy to wavenumber
     def k(self, ch, ene):
         if self.ktype == SIGNS_POS:
-            return self.kpos(ch, ene)
+            return self._kpos(ch, ene)
         elif self.ktype == SIGNS_SPECIFIED:
-            return self.kspecified(ch, ene)
+            return self._kspecified(ch, ene)
         elif self.ktype == SIGNS_BNDANDRES:
-            return self.kbndAndRes(ch, ene)
+            return self._kbndAndRes(ch, ene)
         elif self.ktype == SIGNS_ANA_OVER_AXIS:
-            return self.kanaOverAxis(ch, ene)
+            return self._kanaOverAxis(ch, ene)
         elif self.ktype == SIGNS_ANA_OVER_THRES:
-            return self.kanaOverThres(ch, ene)
+            return self._kanaOverThres(ch, ene)
 
+    # Returns angular momentum for a channel
     def l(self, ch):
         return self.ls[ch]
-
-    def kpos(self, ch, ene):
-        return nw.sqrt(self._getValue(ch, ene))
-
-    def kspecified(self, ch, ene):
-        mult = 1.0
-        if self.ksigns is not None:
-            mult = self.ksigns[ch]
-        return mult * self.kpos(ch, ene)
-
-    def kbndAndRes(self, ch, ene):
-        k = self.kpos(ch, ene)
-        if ene.real <= self.thresholds[ch]: #We want to be on the physical here
-            if ene.imag >= 0.0:
-                sign = 1.0
-            else:
-                sign = -1.0
-        elif ene.real > self.thresholds[ch]: #We want to be on the unphysical here
-            if ene.imag >= 0.0:
-                sign = -1.0
-            else:
-                sign = 1.0
-        return sign*k
-
-    def kanaOverAxis(self, ch, ene):
-        #This keeps function analytical AS LONG as you don't cross thresholds.
-        k = self.kpos(ch, ene)
-        if ene.real <= self.thresholds[ch]: #We want smooth transition over the real axis
-            if ene.imag >= 0.0:
-                sign = 1.0
-            else:
-                sign = -1.0
-        elif ene.real > self.thresholds[ch]: #We want smooth transition over the real axis
-            sign = 1.0
-        return sign*k
-
-    def kanaOverThres(self, ch, ene):
-        #This keeps function analytical AS LONG as you don't cross axis.
-        k = self.kpos(ch, ene)
-        if ene.imag >= 0.0:
-            sign = -1.0
-        else:
-            sign = 1.0
-        return sign*k
-
-    def getPhase(self, ch, ene):
-        if ene.real <= self.thresholds[ch]:
-            return 0.0
-        else:
-            return nw.pi
 
     def getMult(self):
         return self.massMult*REDUCED_MASS
@@ -122,8 +66,56 @@ class calculator:
     def isElastic(self):
         return self.thresholds[1:] == self.thresholds[:-1]
 
+    def _kpos(self, ch, ene):
+        return nw.sqrt(self._getValue(ch, ene))
+
     def _getValue(self, ch, ene):
         #if ene.real < self.thresholds[ch]:
         #    print "WARNING!"
         #    print str(ene) + "   " + str(self.thresholds[ch])
         return self.getMult()*(ene - self.thresholds[ch])
+
+    def _kspecified(self, ch, ene):
+        mult = 1.0
+        if self.ksigns is not None:
+            mult = self.ksigns[ch]
+        return mult * self._kpos(ch, ene)
+
+    def _kbndAndRes(self, ch, ene):
+        k = self._kpos(ch, ene)
+        if ene.real <= self.thresholds[ch]: 
+            # We want to be on the physical here
+            if ene.imag >= 0.0:
+                sign = 1.0
+            else:
+                sign = -1.0
+        elif ene.real > self.thresholds[ch]: 
+            # We want to be on the unphysical here
+            if ene.imag >= 0.0:
+                sign = -1.0
+            else:
+                sign = 1.0
+        return sign*k
+
+    # This keeps function analytical AS LONG as you don't cross thresholds.
+    def _kanaOverAxis(self, ch, ene):
+        k = self._kpos(ch, ene)
+        if ene.real <= self.thresholds[ch]: 
+            #We want smooth transition over the real axis
+            if ene.imag >= 0.0:
+                sign = 1.0
+            else:
+                sign = -1.0
+        elif ene.real > self.thresholds[ch]: 
+            #We want smooth transition over the real axis
+            sign = 1.0
+        return sign*k
+
+    # This keeps function analytical AS LONG as you don't cross axis.
+    def _kanaOverThres(self, ch, ene):
+        k = self._kpos(ch, ene)
+        if ene.imag >= 0.0:
+            sign = -1.0
+        else:
+            sign = 1.0
+        return sign*k
