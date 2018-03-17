438cf529b7635461f1027f5c778abe2dc1798ebe
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:04:11 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'channelutil/README.md')
 
 setup(name='channelutil',
-      version='0.5',
+      version='0.6',
       description='Python package to convert between asymptotic wavenumbers and energies for multichannel, inelastic collisions.',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['channelutil'],
+      package_data={'channelutil': ['tests/*', 'README.md'],
      )
